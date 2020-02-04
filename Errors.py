3a651f577841a23121d70e738a7a237e8547c658
A l <local@local>
2020-02-04 11:40:03 -0500

    Enter

--- Errors.py
+++ Errors.py
@@ -1,12 +1,14 @@
-num_cats = input("How many cats do you have? ")
+#ask user 6 times how many cats they have.
+
 try:
-    if int(num_cats) <= 4 and int(num_cats) >= 0:
-        print ('You dont have that many cats.')
-    elif int(num_cats) < 0:
-        print ('That\'s not right.')
-    else:
-        print ('You have so many cats!')
+    for num_questions in range (6):
+        num_cats = input("How many cats do you have? ")
+        if int(num_cats) <= 4 and int(num_cats) >= 0:
+            print ('You dont have that many cats.')
+        elif int(num_cats) < 0:
+            print ('That\'s not right.')
+        else:
+            print ('You have so many cats!')
 except ValueError:
     print ('You did not enter a number. ')
 
-#comment goes here
